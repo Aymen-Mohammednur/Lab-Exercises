93bb55ff59273696e6d68890086260c723b55db6
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2021-01-24 13:40:37 +0300

    Worked on the bom.js and bom.html files

--- Lab 5/TaskManager/assets/js/app.js
+++ Lab 5/TaskManager/assets/js/app.js
@@ -1,7 +1,61 @@
-// Define UI Variables 
+// Define UI Variables
 
-const taskInput = document.querySelector('#task');             //the task input text field
-const form = document.querySelector('#task-form');             //The form at the top
-const filter = document.querySelector('#filter');              //the task filter text field
-const taskList = document.querySelector('.collection');        //The ul
-const clearBtn = document.querySelector('.clear-tasks');       //the all task clear button
+const taskInput = document.querySelector('#task');
+const form = document.querySelector('#task-form');
+const filter = document.querySelector('#filter');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const reloadIcon = document.querySelector('.fa');
+
+// Adding Event Listeners
+
+form.addEventListener('submit', addNewTask);
+clearBtn.addEventListener('click', clearAllTasks);
+filter.addEventListener('keyup', filterTasks);
+taskList.addEventListener('click', removeTask);
+reloadIcon.addEventListener('click', reloadPage);
+
+// Function Definitions
+
+function addNewTask(e) {
+    if (taskInput.value === "") {
+        taskInput.style.borderColor = "red";
+        return;
+    }
+    const li = document.createElement('li');
+    li.className = "collection-item";
+    const txt = document.createTextNode(taskInput.value);
+    li.appendChild(txt);
+
+    const link = document.createElement('a');
+    link.innerHTML = "<i class= 'fa fa-remove'></i>";
+    link.className = "delete-item secondary-content";
+
+    li.appendChild(link);
+    taskList.appendChild(li);
+
+    e.preventDefault();
+}
+
+function clearAllTasks(e) {
+    // taskList.innerHTML = "";
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function filterTasks(e) {
+    console.log("Task Filter..");
+}
+
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains("delete-item")) {
+        if (confirm("Are you sure about that? ")) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+function reloadPage() {
+    location.reload();
+}
--- Lab 5/TaskManager/assets/js/bom.js
+++ Lab 5/TaskManager/assets/js/bom.js
@@ -18,13 +18,43 @@ Good Luck !!!
 
 
 // Define UI Variables  here 
+const reloadIcon = document.querySelector(".fa");
+const answer = document.querySelectorAll(".badge");
+const href = answer[0], protocol = answer[1], host = answer[2], port = answer[3], hostName = answer[4], pathName = answer[5], Hash = answer[6];
+const appName = answer[7], appVersion = answer[8], platform = answer[9], language = answer[10], cookieEnabled = answer[11], userAgent = answer[12], geoLocation = answer[13];
+const height = answer[14], width = answer[15], pixelDepth = answer[16], colorDepth = answer[17];
+const Length = answer[18], state = answer[19], scrollRest = answer[20];
 
+// Adding Event Listeners
+reloadIcon.addEventListener('click', reloadPage);
 
-
-
-
-
-
-
+// Function Definitions
+function reloadPage() {
+    location.reload();
+}
 
 // Display the BOM Information on the innerHTML of the elements
+href.innerHTML = location.href;
+protocol.innerHTML = location.protocol;
+host.innerHTML = location.host;
+port.innerHTML = location.port;
+hostName.innerHTML = location.hostname;
+pathName.innerHTML = location.pathname;
+Hash.innerHTML = location.hash;
+
+appName.innerHTML = navigator.appName;
+appVersion.innerHTML = navigator.appVersion;
+platform.innerHTML = navigator.platform;
+language.innerHTML = navigator.language;
+cookieEnabled.innerHTML = navigator.cookieEnabled;
+userAgent.innerHTML = navigator.userAgent;
+geoLocation.innerHTML = navigator.geolocation;
+
+height.innerHTML = screen.height;
+width.innerHTML = screen.width;
+pixelDepth.innerHTML = screen.pixelDepth;
+colorDepth.innerHTML = screen.colorDepth;
+
+Length.innerHTML = window.history.length;
+state.innerHTML = window.history.state;
+scrollRest.innerHTML = window.history.scrollRestoration;
