08ba9373ca272605fcfe469625ce138fba3f4e74
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2021-01-26 23:10:40 +0300

    Added calculator using dom

--- Lab 5/CalculatorDOM/assets/js/app.js
+++ Lab 5/CalculatorDOM/assets/js/app.js
@@ -1,32 +1,160 @@
-var num1;
-var num2;
+let input = document.getElementById("input");
+let buttons = document.querySelectorAll(".btn");
+let calculations = document.getElementById("calculation");
+let result = document.getElementById("result");
+
+let i = buttons.length;
+while (i--) {
+    buttons[i].addEventListener("click", main);
+}
+
+// function main(e){
+//     let inputValue = input.value;
+//     let targetName = e.target.name;
+//     if(targetName == "+" || targetName == "-" || targetName == "*" || targetName == "/"|| targetName == "%"|| targetName == "//" || targetName == "pow" || targetName == "sqrt" ){
+//         if(input.value){
+//             if(first_item){
+     
+//                 if(targetName == "pow" || targetName == "sqrt"){
+//                     calculations.value = targetName + " " + String(inputValue)
+//                     results.value = (targetName == "pow" )? Math.pow(Number(inputValue), 2) : Math.sqrt(Number(inputValue))
+//                 }
+//                 else{
+//                     calculations.value = String(inputValue)
+//                     results.value = inputValue
+//                     }
+//                 input.value = ""
+//                 first_item = false
+//             }
+//             else{
+//                 if(targetName == "pow" || targetName == "sqrt"){
+//                     calculations.value = targetName + " " + String(inputValue)
+//                 }
+//                 else{
+//                     calculations.value = calculations.value + targetName + String(inputValue)
+//                 }
+//                 input.value = ""
+            
+//                 switch(targetName){
+//                     case "+":
+//                         results.value = add([results.value, inputValue])
+//                         break;
+//                     case "-":
+//                         results.value = sub(results.value, inputValue)
+//                         break;
+//                     case "*":
+//                         results.value = mult([results.value, inputValue])
+//                         break;
+//                     case "/":
+//                         results.value = div(results.value, inputValue)
+//                         break;
+//                     case "%":
+//                         results.value = mod(results.value, inputValue)
+//                         break;
+//                     case "//":
+//                         results.value = int_div(results.value, inputValue)
+//                         break;
+//                     case "pow":
+//                         results.value = Math.pow(Number(results.value), 2)
+//                         break;
+//                     case "sqrt":
+//                         results.value = Math.sqrt(Number(results.value))
+//                         break;
+//                 }
+//             }
+//         }
+//     }
+//     else{
+//         if(targetName == "c"){
+//             first_item = true;
+//             calculations.value = "";
+//             results.value = "";
+//         }
+//         else{
+//             if(first_item){
+//                 calculations.value = String(inputValue)
+//                 results.value = inputValue
+//                 first_item = false
+//             }
+//             else{
+//                 alert("Your result is shown below or you haven't chosen operation")
+//             }
+//         }
+//         input.value = ""
+//     }
+
+// }
+
+function main(e) {
+    let inputValue = input.value;
+    let operation = e.target.name;
+    if (operation == '+' || operation == '-' || operation == '*' || operation == '/' || operation == 'pow' || operation == 'sqrt' || operation == '%') {
+        if (operation == "pow") {
+            result.value = square(inputValue);
+            //input.value = "";
+        }
+        else if (operation == "sqrt") {
+            result.value = Math.sqrt(Number(inputValue));
+            //input.value = "";
+        }
+        else if (operation == "+") {
+            result.value = add([result.value, inputValue]);
+            //input.value = "";
+        }
+        else if (operation == "*") {
+            result.value = mul([result.value, inputValue]);
+            //input.value = "";
+        }
+        else if (operation == "-") {
+            result.value = sub(result.value, inputValue);
+            //input.value = "";
+        }
+        else if (operation == "/") {
+            result.value = div(result.value, inputValue);
+            //input.value = "";
+        }
+        else if (operation == "%") {
+            result.value = modulous(result.value, inputValue);
+            //input.value = "";
+        }
+        else {
+            result.value = inputValue;
+        }
+    }
+    else if (operation == 'ac') {
+        result.value = "";
+    }
+    else if (operation == 'equals') {
+        result.value = inputValue;
+    }
+}
 
 function add(arr) {
-    var result = 0;
+    let result = 0;
     for(let i = 0; i < arr.length; i++) {
-        result = result + parseInt(arr[i]);
+        result += Number(arr[i]);
     }
     return result;
 }
 
 function sub(num1, num2) {
-    var result = 0;
-    result = num1 - num2;
+    let result = 0;
+    result = Number(num1) - Number(num2);
     return result;
 }
 
 function mul(arr) {
-    var result = 1;
+    let result = 1;
     for( var i = 0; i < arr.length; i++) {
-        result = result * parseInt(arr[i]);
+        result *= Number(arr[i]);
     }
     return result;
 }
 
 function div(num1, num2) {
-    var result = 0;
+    let result = 0;
     if (num2 != 0) {
-        result = num1 / num2;
+        result = Number(num1) / Number(num2);
         return result;
     }
     else {
@@ -34,123 +162,16 @@ function div(num1, num2) {
     }
 }
 
-average = (arr) => {
-    var sum = add(arr);
-    result = sum / parseInt(arr.length);
-    return result;
-}
-
 square = (num) => {
     return num * num;
 }
 
-MinMax = (arr) => {
-    let min = arr[0], max = arr[0]
-  
-    for (let i = 1; i < arr.length; i++) {
-      let value = arr[i]
-      min = (value < min) ? value : min
-      max = (value > max) ? value : max
-    }
-  
-    return [min, max]
-}
-
-(function main() {
-    let arr = new Array(); 
-    let operand = prompt("Enter Your Operation \n '1' for Addition \n '2' for Subtraction \n '3' for Multiplicaton \n '4' for Division \n '5' for Average \n '6' for Square \n '7' for Min and Max \n '8' to Quit ");
-    if (isNaN(Number(operand))) {
-        console.log("Invalid, Try Again!")
-        main();
+let modulous = (num1, num2) => {
+    if (num2 == 0) {
+        return "Can't divide by 0!";
     }
     else {
-        if (operand == "1" || operand == "3" || operand == "5" || operand == "7") {
-            var amountOfNum = prompt("How many numbers do you want to operate on: ");
-            if (isNaN(Number(amountOfNum))) {
-                console.log("Input numbers only!");
-                main();
-            }
-            else {
-                for (let i = 0; i < Number(amountOfNum); i++) {
-                    arr[i] = prompt("Your Number " + (i + 1));
-                    if (isNaN(Number(arr[i]))) {
-                        console.log("Input numbers only!");
-                        main();
-                    }
-                }
-                if (operand == "1") {
-                    result = add(arr);
-                    console.log(result);
-                    main();
-                }
-                else if (operand == "3") {
-                    result = mul(arr);
-                    console.log(result);
-                    main();
-                }
-                else if (operand == "5") {
-                    result = average(arr);
-                    console.log("Average is " + result);
-                    main();
-                }
-                else if (operand == "7") {
-                    let [min, max] = MinMax(arr);
-                    console.log(`Minimum: ${min}, Maximum: ${max}`);
-                    main();
-                }
-                else {
-                    console.log("Invalid");
-                    main();
-                }
-            }
-        }
-        else if (operand == "2" || operand == "4") {
-            if (operand == "2") {
-                var num1 = prompt("Enter first number ");
-                var num2 = prompt("Enter second number ");
-                if (isNaN(num1) || isNaN(num2)) {
-                    console.log("Input numbers only!")
-                    main();
-                }
-                else {
-                    result = sub(Number(num1), Number(num2));
-                    console.log(result);
-                    main();
-                }        
-            }
-            else if (operand == "4") {
-                var num1 = prompt("Enter first number ");
-                var num2 = prompt("Enter second number ");
-                if (isNaN(num1) || isNaN(num2)) {
-                    console.log("Input numbers only!")
-                    main();
-                }
-                else {
-                    result = div(Number(num1), Number(num2));
-                    console.log(result);
-                    main();
-                }
-                
-            }
-        }
-        else if (operand == "6") {
-            var num = prompt("Enter the number ");
-            if (isNaN(num)) {
-                console.log("Input numbers only!")
-                main();
-            }
-            else {
-                console.log("The square of " + num + " is " + square(num));
-                main();
-            }
-        }
-        else if (operand == "8") {
-            console.log("Goodbye");
-        }
-        else {
-            console.log("Try Again");
-            main();
-        }
+        let result = (Number(num1)) % (Number(num2));
+        return result;
     }
-
-})();
+}
