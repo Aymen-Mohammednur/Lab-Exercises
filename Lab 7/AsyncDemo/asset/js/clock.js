27eed07a4fa58c30b57765a916bd7919a9469455
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2021-02-18 09:55:08 +0300

    OOP1.js finished

--- Lab 7/AsyncDemo/asset/js/clock.js
+++ Lab 7/AsyncDemo/asset/js/clock.js
@@ -8,15 +8,15 @@ function setClock() {
     const myDate = new Date();
 
     const seconds = myDate.getSeconds();
-    const secRot = (((seconds / 60) * 360) + 90);
+    const secRot = ((seconds / 60) * 360) + 90;
     secondDiv.style.transform = `rotate(${secRot}deg)`
 
     const minutes = myDate.getMinutes();
-    const minRot = (((minutes / 60) * 360) + 90);
+    const minRot = ((minutes / 60) * 360) + 90;
     minuteDiv.style.transform = `rotate(${minRot}deg)`
 
     const hours = myDate.getHours();
-    const houRot = (((hours / 60) * 360) + 90);
+    const houRot = ((hours / 60) * 360) + 90;
     hourDiv.style.transform = `rotate(${houRot}deg)`
 
 }
